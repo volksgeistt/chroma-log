08e94157c1672f71f03501d00133b713cf3374ce
volksgeistt <131983323+volksgeistt@users.noreply.github.com>
2024-08-28 20:34:19 +0530

    Create chroma-log-init.py

---
+++ chroma-log-init.py
@@ -0,0 +1,4 @@
+from .chromaLog import ChromaLog, debug, info, warning, error, critical, colorize
+
+__all__ = ['ChromaLog', 'debug', 'info', 'warning', 'error', 'critical', 'colorize']
+__version__ = "0.1.0"
